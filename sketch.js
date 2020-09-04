585551d0b2ee62c0dad0239fdd6d1eec8ee77999
VirSanghvi <68021018+VirSanghvi@users.noreply.github.com>
2020-09-04 20:08:04 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -32,12 +32,12 @@ var damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
 
 if(damage>10)
 {
-wall.shapeColor=(255,0,0);
+wall.shapeColor=color(255,0,0);
 }
 
 if(damage<10)
 {
-wall.shapeColor=(0,255,0);
+wall.shapeColor=color(0,255,0);
 }
 
 }
